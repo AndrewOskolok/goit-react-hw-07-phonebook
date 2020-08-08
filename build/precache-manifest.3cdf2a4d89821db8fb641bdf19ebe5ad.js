49887d4f5ef44aab086390c2a81e1504f2c37055
Oscolok <oscolok7@gmail.com>
2020-08-08 13:23:59 +0300

    hw 7 is done

---
+++ build/precache-manifest.3cdf2a4d89821db8fb641bdf19ebe5ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec6074e05b32a6f8d3b0f453a85a1c06",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "e7e3fae90413e81fb585",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.2cf06d61.chunk.css"
+  },
+  {
+    "revision": "bc02dae3545b86b042fd",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.6ed1cf19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.6ed1cf19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e3fae90413e81fb585",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.13e10f38.chunk.js"
+  },
+  {
+    "revision": "811568ded829eb4f092f",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.683bdc55.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,45 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import AddContact from "./components/AddContact/AddContact";
+import AllContacts from "./components/AllContacts/AllContacts";
+import FilterContacts from "./components/FilterContacts/FilterContacts";
+import Alert from "./components/Alert/Alert";
+import { CSSTransition } from "react-transition-group";
+import { useSelector, useDispatch } from "react-redux";
+import { getUserOperation } from "./redux/operation/itemsOperation";
+
+function App() {
+  const [showAlert, setShowAlert] = useState({ status: false, text: "" });
+
+  const contacts = useSelector((state) => state.items);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUserOperation());
+  }, [dispatch]);
+
+  return (
+    <div className="container">
+      <CSSTransition in={true} appear={true} timeout={500} classNames="title">
+        <h1 className="title">Phonebook</h1>
+      </CSSTransition>
+      <AddContact setShowAlert={setShowAlert} />
+      {contacts.length >= 2 && <FilterContacts />}
+
+      {contacts.length > 0 && <AllContacts />}
+
+      <CSSTransition
+        in={showAlert.status}
+        timeout={250}
+        classNames="alert"
+        mountOnEnter
+        unmountOnExit
+      >
+        <Alert showAlert={showAlert.text} />
+      </CSSTransition>
+    </div>
+  );
+}
+
+export default App;
